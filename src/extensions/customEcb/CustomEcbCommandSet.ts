7f2843a36b282eca5176c5ba715204fb49af835f
Arpula Lakshmi Prasad <Arpula.Lakshmi-Prasad-ext@osram-os.com>
2021-12-10 21:46:19 +0800

    added code for source page checking

--- src/extensions/customEcb/CustomEcbCommandSet.ts
+++ src/extensions/customEcb/CustomEcbCommandSet.ts
@@ -32,7 +32,7 @@ import { Navigation } from "@pnp/sp/navigation";
 import { ITranslationService } from "../../services/ITranslationService";
 import { TranslationService } from "../../services/TranslationService";
 import { environment } from '../../environments/environment';
-import { SPPermission} from '@microsoft/sp-page-context';
+import { SPPermission } from '@microsoft/sp-page-context';
 
 /**
  * If your command set uses the ClientSideComponentProperties JSON input,
@@ -61,21 +61,21 @@ export default class CustomEcbCommandSet extends BaseListViewCommandSet<ICustomE
         const langFeature: boolean = await this.getMultiLingualFeatureEnabled();
         //check if page is only the main page and nothing other then aspx page
         const validPage = (pageName): boolean => {
-            return pageName.slice(10, pageName.lastIndexOf('/')).length === 0 && pageName.indexOf('.aspx') !== -1 ?  true :  false;
+            return pageName.slice(10, pageName.lastIndexOf('/')).length === 0 && pageName.indexOf('.aspx') !== -1 ? true : false;
         }
 
         const compareOneCommand: Command = this.tryGetCommand('ShowDetails');
         if (compareOneCommand) {
             //TODO now checing for editlistitems permission.
-           // console.log(this.context.pageContext.list.permissions.hasPermission(SPPermission.editListItems));
+            // console.log(this.context.pageContext.list.permissions.hasPermission(SPPermission.editListItems));
 
             if (event.selectedRows.length == 1) {
                 //let pagename = event.selectedRows[0].getValueByName('FileLeafRef');
                 //Dialog.alert(pagename);
 
-                    // This command should be hidden unless exactly one row is selected.
-                    const pageName: string = event.selectedRows[0].getValueByName("FileRef");
-                    compareOneCommand.visible = event.selectedRows.length === 1 && langFeature && validPage(pageName); ;
+                // This command should be hidden unless exactly one row is selected.
+                const pageName: string = event.selectedRows[0].getValueByName("FileRef");
+                compareOneCommand.visible = event.selectedRows.length === 1 && langFeature && validPage(pageName);;
             }
         }
     }
@@ -93,38 +93,29 @@ export default class CustomEcbCommandSet extends BaseListViewCommandSet<ICustomE
     }
 
 
-
-
     private _onTranslate = (languagecode: string): void => {
-
         console.log('_onTranslate');
 
-        const targetRelativePageUrl: string = '/SitePages/' + languagecode + '/' + this._pageName;
-
         (async () => {
-
-            //Dialog.alert(this._pageName);
             try {
                 console.log('Copying......... ');
-                //const result = await sp.web.loadClientsidePage(deRelativePageUrl);
-                let targetpage: IClientsidePage = undefined;
+                const sourceRelativePageUrl: string = '/SitePages/' + this._pageName;
+                let sourcepage: IClientsidePage = undefined;
                 try {
-                    targetpage = await ClientsidePageFromFile(sp.web.getFileByServerRelativeUrl(targetRelativePageUrl));
+                    sourcepage = await ClientsidePageFromFile(sp.web.getFileByServerRelativeUrl(sourceRelativePageUrl));
                 } catch (error) {
                     console.dir(error);
-                    console.log('target page not found');
-                    Dialog.alert('Language page not found,Please contact admin....');
-
+                    console.log('source page not found ' + this._pageName);
+                    Dialog.alert('Source page ' + this._pageName + ' not found in [Site Pages] Library,Please contact admin....');
                 }
-                //console.log('async/await source -> ', sourcepage);
+                console.log('async/await source -> ', sourcepage);
 
-                if (targetpage != undefined) {
-                    
+                if (sourcepage != undefined) {
+
                     if (confirm('Are you sure you want to translate this page')) {
 
-                        const sourceRelativePageUrl: string = '/SitePages/' + this._pageName;
-                        const sourcepage = await ClientsidePageFromFile(sp.web.getFileByServerRelativeUrl(sourceRelativePageUrl));
-                        console.log('async/await target -> ', targetpage);
+                        const targetRelativePageUrl: string = '/SitePages/' + languagecode + '/' + this._pageName;
+                        const targetpage = await ClientsidePageFromFile(sp.web.getFileByServerRelativeUrl(targetRelativePageUrl));
                         await sourcepage.copyTo(targetpage, true);
 
                         console.log('Copy Completed.......');
@@ -135,15 +126,12 @@ export default class CustomEcbCommandSet extends BaseListViewCommandSet<ICustomE
 
                         Dialog.alert(`Copy Completed.......Starting Translation.`);
 
-                        await new Promise(resolve => setTimeout(resolve, 6000));
+                        await new Promise(resolve => setTimeout(resolve, 5000));
 
                         sp.web.loadClientsidePage(targetRelativePageUrl).then(async (clientSidePage: IClientsidePage) => {
 
                             try {
                                 console.log('translation started');
-
-                                //clientSidePage.title = this._getTranslatedText(clientSidePage.title, languagecode, false);
-
 
                                 var clientControls: ColumnControl<any>[] = [];
                                 clientSidePage.findControl((c) => {
@@ -156,6 +144,8 @@ export default class CustomEcbCommandSet extends BaseListViewCommandSet<ICustomE
                                     return false;
                                 });
 
+                                await this._alltranslateClientSideControl(translationService, clientControls, languagecode);
+
                                 //const nav = sp.web.navigation.topNavigationBar;
                                 //Dialog.alert(nav.length.toString());
                                 //const childrenData = await nav.getById(1).children();
@@ -163,11 +153,11 @@ export default class CustomEcbCommandSet extends BaseListViewCommandSet<ICustomE
                                 //    Title: "A new title",
                                 //});
 
-                                await this._alltranslateClientSideControl(translationService, clientControls, languagecode);
-
-                                console.log('translation complete');
+                                //clientSidePage.title = this._getTranslatedText(clientSidePage.title, languagecode, false);
 
                                 clientSidePage.save();
+
+                                console.log('translation complete');
 
                                 Dialog.alert(`Translation Completed........`);
 
@@ -193,34 +183,33 @@ export default class CustomEcbCommandSet extends BaseListViewCommandSet<ICustomE
     }
 
     private _alltranslateClientSideControl = async (translationService: ITranslationService, clientsideControls: ColumnControl<any>[], languagecode: string): Promise<void> => {
-
-        for (const c of clientsideControls) {
-            if (c instanceof ClientsideWebpart) {
-                // console.log(c);
-                if (c.data.webPartData?.serverProcessedContent?.searchablePlainTexts) {
-                    let propkeys = Object.keys(c.data.webPartData?.serverProcessedContent?.searchablePlainTexts);
-                    //console.log(propkeys.length + "    " + propkeys);
-                    for (const key of propkeys) {
-                        //console.log("Translation for key ... " + key);
-                        const propvalue = c.data.webPartData?.serverProcessedContent?.searchablePlainTexts[key];
-                        if (propvalue) {
-                            //console.log("Translation for value ... " + propvalue);
-                            let translationResult = await translationService.translate(propvalue, languagecode, false);
-                            const translatedText = translationResult.translations[0].text;
-                            c.data.webPartData.serverProcessedContent.searchablePlainTexts[key] = translatedText;
+        try {
+            for (const c of clientsideControls) {
+                if (c instanceof ClientsideWebpart) {
+                    if (c.data.webPartData?.serverProcessedContent?.searchablePlainTexts) {
+                        let propkeys = Object.keys(c.data.webPartData?.serverProcessedContent?.searchablePlainTexts);
+                        for (const key of propkeys) {
+                            const propvalue = c.data.webPartData?.serverProcessedContent?.searchablePlainTexts[key];
+                            if (propvalue) {
+                                let translationResult = await translationService.translate(propvalue, languagecode, false);
+                                const translatedText = translationResult.translations[0].text;
+                                c.data.webPartData.serverProcessedContent.searchablePlainTexts[key] = translatedText;
+                            }
                         }
-
+                    }
+                }
+                else if (c instanceof ClientsideText) {
+                    const propvalue = c.text;
+                    if (propvalue) {
+                        let translationResult = await translationService.translate(propvalue, languagecode, true);
+                        const translatedText = translationResult.translations[0].text;
+                        c.text = translatedText;
                     }
                 }
             }
-            else if (c instanceof ClientsideText) {
-                const propvalue = c.text;
-                if (propvalue) {
-                    let translationResult = await translationService.translate(propvalue, languagecode, true);
-                    const translatedText = translationResult.translations[0].text;
-                    c.text = translatedText;
-                }
-            }
+        } catch (err) {
+            console.dir('aynsc error');
+            console.log(err);
         }
     }
 
@@ -240,27 +229,27 @@ export default class CustomEcbCommandSet extends BaseListViewCommandSet<ICustomE
     //        //    let translationResult = await translationService.translate(text, languagecode, asHtml);
     //        //    translatedText = translationResult.translations[0].text
 
-        //console.log('end');
-        
-        //return translatedText;
+    //console.log('end');
+
+    //return translatedText;
     //}
     //*************Function to get Multilingual Feature Enabled************************************* */
-    public getMultiLingualFeatureEnabled = () : Promise<boolean> => {
+    public getMultiLingualFeatureEnabled = (): Promise<boolean> => {
         return new Promise<boolean>(async (resolve, reject) => {
             let features = await sp.web.features.select("DisplayName", "DefinitionId").get().then(f => {
-                if(_.find(f, { "DisplayName": "MultilingualPages" })){
+                if (_.find(f, { "DisplayName": "MultilingualPages" })) {
                     return resolve(true);
                 }
-                else{
-                    return resolve(false);  
+                else {
+                    return resolve(false);
                 }
-            
-        }).catch(error => {
-            console.log(error);
-            return reject(false);
-        });
-        return resolve(false)
-    })
+
+            }).catch(error => {
+                console.log(error);
+                return reject(false);
+            });
+            return resolve(false)
+        })
 
     }
 

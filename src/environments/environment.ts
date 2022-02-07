4176c4473a95624d39b1b7b9a343d8a87a783388
pratap215 <styleepop@gmail.com>
2022-02-07 14:20:34 +0530

    new api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,8 @@ export const environment = {
     //translatorServiceUrl: 'https://translationapplicationcoreapi.azurewebsites.net/api/TranslatorService/',
     //    environmentName: 'dwpstage',
         regionSpecifier: "",
-        translatorApiKey: "3f272905281e48cc9e539d881729dab2",
+        //translatorApiKey: "3f272905281e48cc9e539d881729dab2",
+        translatorApiKey: "583852e1369d4f0caf4928a6457bfcef",
 
 
   }
